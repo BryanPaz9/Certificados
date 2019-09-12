90819bfa947ad6a27c317b099f3cb2e9a0b14481
BryanPaz9 <bryanascuc7@gmail.com>
2019-09-12 15:57:40 -0600

    Update to OraclePage

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var img = document.getElementById('img1');
 function modalView(id){
     if (id == '1'){
         console.log('1');
-        img.src= '../assets/img/academik/campos obligatorios.PNG';
+        img.src= '../assets/img/oracle/campos obligatorios.PNG';
     }else if(id == '2'){
         console.log('2');
         img.src = '../assets/img/academik/campos vacios.PNG';
@@ -17,5 +17,29 @@ function modalView(id){
         img.src = '../assets/img/academik/generatedAcademik.PNG'
     }else if(id == '5'){
         img.src = '../assets/img/academik/certificate.PNG';
+    }
+}
+
+function modalViewOracle(id){
+    if (id == '1'){
+        console.log('1');
+        img.src= '../assets/img/oracle/controles.PNG';
+    }else if(id == '2'){
+        console.log('2');
+        img.src = '../assets/img/oracle/campos.PNG';
+    }else if(id =='3'){
+        console.log('3');
+        img.src = '../assets/img/oracle/director.PNG'
+    }else if(id == '4'){
+        console.log('4');
+        img.src = '../assets/img/oracle/firma.PNG'
+    }else if(id == '5'){
+        img.src = '../assets/img/oracle/registro.PNG';
+    }else if(id == '6'){
+        img.src = '../assets/img/oracle/generate.PNG'
+    }else if( id == '7'){
+        img.src = '../assets/img/oracle/certificate.PNG'
+    }else if(id == '8'){
+        img.src = '../assets/img/oracle/without.PNG'
     }
 }
