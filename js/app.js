438650734be9010af38ef46c92a780dc7b5054a4
BryanPaz9 <bryanascuc7@gmail.com>
2019-09-13 08:53:26 -0600

    first update 13/09/2019

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 'use strict'
 var id;
 var img = document.getElementById('img1');
+var img2 = document.getElementById('img2');
 
 function modalView(id){
     if (id == '1'){
         console.log('1');
-        img.src= '../assets/img/oracle/campos obligatorios.PNG';
+        img.src= '../assets/img/academik/campos obligatorios.PNG';
     }else if(id == '2'){
         console.log('2');
         img.src = '../assets/img/academik/campos vacios.PNG';
@@ -23,23 +24,31 @@ function modalView(id){
 function modalViewOracle(id){
     if (id == '1'){
         console.log('1');
-        img.src= '../assets/img/oracle/controles.PNG';
+        img2.src= '../assets/img/oracle/controles.PNG';
     }else if(id == '2'){
         console.log('2');
-        img.src = '../assets/img/oracle/campos.PNG';
+        img2.src = '../assets/img/oracle/campos.PNG';
     }else if(id =='3'){
         console.log('3');
-        img.src = '../assets/img/oracle/director.PNG'
+        img2.src = '../assets/img/oracle/director.PNG'
     }else if(id == '4'){
         console.log('4');
-        img.src = '../assets/img/oracle/firma.PNG'
+        img2.src = '../assets/img/oracle/firma.PNG'
     }else if(id == '5'){
-        img.src = '../assets/img/oracle/registro.PNG';
+        img2.src = '../assets/img/oracle/registro.PNG';
     }else if(id == '6'){
-        img.src = '../assets/img/oracle/generate.PNG'
+        img2.src = '../assets/img/oracle/generate.PNG'
     }else if( id == '7'){
-        img.src = '../assets/img/oracle/certificate.PNG'
+        img2.src = '../assets/img/oracle/certificate.PNG'
     }else if(id == '8'){
-        img.src = '../assets/img/oracle/without.PNG'
+        img2.src = '../assets/img/oracle/without.PNG'
+    }
+}
+
+function modalViewNabenik(id){
+    if (id == '1'){
+        img.src = '../assets/img/nabenik/.PNG';
+    }else if(id == '2'){
+        img.src = '../assets/img/nabenik/.PNG';
     }
 }
