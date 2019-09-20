ea94d03850f36e0e1d70e8cf711f89cfa050c5db
BryanPaz9 <bryanascuc7@gmail.com>
2019-09-20 09:46:27 -0600

    Clean js code!

--- js/app.js
+++ js/app.js
@@ -6,17 +6,13 @@ var img3 = document.getElementById('img3');
 
 function modalView(id){
     if (id == '1'){
-        console.log('1');
         img.src= '../assets/img/academik/campos obligatorios.PNG';
     }else if(id == '2'){
-        console.log('2');
         img.src = '../assets/img/academik/campos vacios.PNG';
     }else if(id =='3'){
-        console.log('3');
-        img.src = '../assets/img/academik/registro.PNG'
+        img.src = '../assets/img/academik/registro.PNG';
     }else if(id == '4'){
-        console.log('4');
-        img.src = '../assets/img/academik/generatedAcademik.PNG'
+        img.src = '../assets/img/academik/generatedAcademik.PNG';
     }else if(id == '5'){
         img.src = '../assets/img/academik/certificate.PNG';
     }
@@ -24,41 +20,33 @@ function modalView(id){
 
 function modalViewOracle(id){
     if (id == '1'){
-        console.log('1');
         img2.src= '../assets/img/oracle/controles.PNG';
     }else if(id == '2'){
-        console.log('2');
         img2.src = '../assets/img/oracle/campos.PNG';
     }else if(id =='3'){
-        console.log('3');
-        img2.src = '../assets/img/oracle/director.PNG'
+        img2.src = '../assets/img/oracle/director.PNG';
     }else if(id == '4'){
-        console.log('4');
-        img2.src = '../assets/img/oracle/firma.PNG'
+        img2.src = '../assets/img/oracle/firma.PNG';
     }else if(id == '5'){
         img2.src = '../assets/img/oracle/registro.PNG';
     }else if(id == '6'){
-        img2.src = '../assets/img/oracle/generate.PNG'
+        img2.src = '../assets/img/oracle/generate.PNG';
     }else if( id == '7'){
         img2.src = '../assets/img/oracle/certificate.PNG'
     }else if(id == '8'){
-        img2.src = '../assets/img/oracle/without.PNG'
+        img2.src = '../assets/img/oracle/without.PNG';
     }
 }
 
 function modalViewNabenik(id){
     if (id == '1'){
-        console.log('1');
         img3.src= '../assets/img/nabenik/controls.PNG';
     }else if(id == '2'){
-        console.log('2');
         img3.src = '../assets/img/nabenik/obligatorio.PNG';
     }else if(id =='3'){
-        console.log('3');
-        img3.src = '../assets/img/nabenik/registro.PNG'
+        img3.src = '../assets/img/nabenik/registro.PNG';
     }else if(id == '4'){
-        console.log('4');
-        img3.src = '../assets/img/nabenik/generated.PNG'
+        img3.src = '../assets/img/nabenik/generated.PNG';
     }else if(id == '5'){
         img3.src = '../assets/img/nabenik/certificate.PNG';
     }
